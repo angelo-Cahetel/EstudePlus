4c21adb87b846cd185c8bcf0e03cf93f8ebc586c
Angelo Mendes <angelo030825@gmail.com>
2025-06-17 20:02:16 -0300

    Initial Commit

---
+++ EstudePlus/EstudePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  EstudePlusApp.swift
+//  EstudePlus
+//
+//  Created by Ângelo Mendes on 17/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EstudePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
